02f1862dd78fe94a8608896913d04b4fe6f70c92
ganatan <dannyganatan@gmail.com>
2020-03-05 18:46:39 +0100

    Update Unit and e2e tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-import { ReactiveFormsModule } from '@angular/forms';
-
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        ReactiveFormsModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
       ],
     }).compileComponents();
   }));
@@ -21,16 +18,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-example-reactive-form'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-example-reactive-form');
-  });
+  /*  it(`should have as title 'angular-example-reactive-form'`, () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      expect(app.title).toEqual('angular-example-reactive-form');
+    }); */
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-example-reactive-form app is running!');
-  });
 });
--- src/app/example-reactive-form/example-reactive-form.component.spec.ts
+++ src/app/example-reactive-form/example-reactive-form.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExampleReactiveFormComponent } from './example-reactive-form.component';
+import { FormBuilder } from '@angular/forms';
+import { PrettyJsonPipe } from './pretty-json.pipe';
 
 describe('ExampleReactiveFormComponent', () => {
   let component: ExampleReactiveFormComponent;
@@ -8,9 +10,15 @@ describe('ExampleReactiveFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ExampleReactiveFormComponent ]
+      declarations: [
+        ExampleReactiveFormComponent,
+        PrettyJsonPipe
+      ],
+      providers: [
+        FormBuilder
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
